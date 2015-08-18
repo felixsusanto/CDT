0fbebf6365bd6956a4cfd25fb462ec10931600f6
felixsusanto <felix.susanto@wds.co>
2015-08-18 18:07:07 +0800

    major updates

---
+++ gruntfile.js
@@ -0,0 +1,71 @@
+// Gruntfile with the configuration of grunt-express and grunt-open. No livereload yet!
+module.exports = function(grunt) {
+ 
+  // Load Grunt tasks declared in the package.json file
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+ 
+  // Configure Grunt 
+  grunt.initConfig({
+    
+    //grunt-contrib-compass to compile scss file to dist files
+    compass: {                  // Task
+      dist: {                   // Target
+        options: {              // Target options
+          sassDir: '_scss',
+          cssDir: 'css',
+          environment: 'production'
+        }
+      }
+    },
+    svgstore: {
+      options: {
+        prefix : 'shape-', // This will prefix each <g> ID
+        svg: { style: "display:none;"}
+      },
+      default: {
+        files: {
+          '_includes/svg-store.html': ['_svgs/*.svg'],
+        }
+      }
+    },
+    shell: {
+      jekyllServe:{
+        command: "jekyll serve --baseurl ''"
+      },
+      jekyllBuild:{
+        command: 'jekyll build --config _config-dev.yml'
+      }
+    },
+
+    //grunt-watch will monitor the project files
+    watch: {
+      options:{
+        livereload: true
+      },
+      site: {
+        files: ['**/*.html', 'index.html', '**/*.md', '!_site/**/*'],
+        tasks: ['shell:jekyllBuild'],
+      },
+      css: {
+        files: ['_scss/*.scss', '!_site/**/*'],
+        tasks: ['compass', 'shell:jekyllBuild'],
+      },
+      svgs: {
+        files: ['_svgs/*.svg', '!_site/**/*'],
+        tasks: ['svgstore', 'shell:jekyllBuild']
+      }
+    },
+    // grunt-open will open your browser at the project's URL
+    open: {
+      all: {
+        // Gets the port from the connect configuration
+        path: 'http://localhost:4000/index.html'
+      }
+    }
+  });
+ 
+  // Creates the `server` task
+  grunt.registerTask('serve', ['shell:jekyllServe']);
+  //Compile scss task
+  grunt.registerTask('default', ['compass','svgstore', 'shell:jekyllBuild','open', 'watch']);
+};
